36cc1878da02d1684e090658e0bdbdd1cc750416
Emily Asch <e.asch@me.com>
2018-07-21 18:13:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function happyHolidayTo (holiday, name){
 }
 
 function holidayCountdown (days, holiday){
-  return ("${days}" days until "${holiday}"!)
+  return (`It's ${days} days until ${holiday}!`)
 }
 
