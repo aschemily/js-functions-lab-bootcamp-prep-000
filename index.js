2add5eda19052c89010e9b3f4f786fcc0be636bd
Emily Asch <e.asch@me.com>
2018-07-21 18:12:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function happyHolidaysTo(name){
 
 var name = "Bob";
 var holiday = "Christmas";
-function happyHolidaysTo (){
+function happyHolidayTo (){
   return ('Happy ${holiday}, ${name}!')
 };
 
