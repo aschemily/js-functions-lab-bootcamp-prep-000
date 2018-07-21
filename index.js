6d38a7aad1335bd53307542db4683dfc4a7ef85a
Emily Asch <e.asch@me.com>
2018-07-21 17:59:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 // write your code below!
-function happHolidaysTo (){
+function happyHolidaysTo (){
 return('Happy Holidays!')
 }
+
+var name = "Joe";
+function happyHolidaysTo (){
+  return ('Happy holidays, ${name}!')
+};
+
+var name = "Bob";
+var holiday = "Christmas";
+function happyHolidaysTo (){
+  return ('Happy ${holiday}, ${name}!')
+};
+
+var days = "12"
+function holidayCountdown (){
+  return ("${days}" days until "${holiday}"!)
+};
+
